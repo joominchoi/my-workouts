0486d509dbe8936fdfadf6a536c33fae5d1ac194
JooMin Choi <joomin.choi@clearscore.com>
2023-05-15 14:37:40 +0100

    created SummaryView that shows summary of workout

---
+++ MyWorkouts Watch App/SummaryView.swift
@@ -0,0 +1,83 @@
+//
+//  SummaryView.swift
+//  MyWorkouts Watch App
+//
+//  Created by JooMin Choi on 15/05/2023.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    @State private var durationFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.zeroFormattingBehavior = .pad
+        return formatter
+    }()
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading) {
+                SummaryMetricView(
+                    title: "Total Time",
+                    value: durationFormatter.string(from: 30 * 60 + 15) ?? ""
+                ).accentColor(Color.yellow)
+                SummaryMetricView(
+                    title: "Total Distance",
+                    value: Measurement(
+                        value: 1625,
+                        unit: UnitLength.meters
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .road
+                        )
+                    )
+                ).accentColor(Color.green)
+                SummaryMetricView(
+                    title: "Total Energy",
+                    value: Measurement(
+                        value: 96,
+                        unit: UnitEnergy.kilocalories
+                    ).formatted(
+                        .measurement(
+                            width: .abbreviated,
+                            usage: .workout
+                        )
+                    )
+                ).accentColor(Color.pink)
+                SummaryMetricView(
+                    title: "Avg. Heart Rate",
+                    value: 143
+                        .formatted()
+                    + " bpm"
+                ).accentColor(Color.red)
+                Button("Done") {
+                }
+            }
+            .scenePadding()
+        }
+        .navigationTitle("Summary")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
+
+struct SummaryMetricView: View {
+    var title: String
+    var value: String
+    
+    var body: some View {
+        Text(title)
+        Text(value)
+            .font(.system(.title2, design: .rounded).lowercaseSmallCaps()
+            )
+            .foregroundColor(.accentColor)
+        Divider()
+    }
+}
