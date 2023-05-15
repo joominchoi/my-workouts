7147a503af81dc03bace67bf6527962c9d1dd1a4
JooMin Choi <joomin.choi@clearscore.com>
2023-05-15 10:55:15 +0100

    Initial Commit

---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by JooMin Choi on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
