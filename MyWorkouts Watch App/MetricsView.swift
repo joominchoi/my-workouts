836870912624fc01350505101ce5e68ba2ca31d0
JooMin Choi <joomin.choi@clearscore.com>
2023-05-16 16:25:17 +0100

    added functionality to metrics view that measures workout time and simulates other metrics

--- MyWorkouts Watch App/MetricsView.swift
+++ MyWorkouts Watch App/MetricsView.swift
@@ -8,51 +8,50 @@
 import SwiftUI
 
 struct MetricsView: View {
+    @EnvironmentObject var workoutManager: WorkoutManager
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            ElapsedTimeView(
-                elapsedTime: 3 * 60 + 15.24,
-                showSubseconds: true
-            ).foregroundColor(Color.yellow)
-            Text(
-                Measurement(
-                    value: 47,
-                    unit: UnitEnergy.kilocalories
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .workout
-                    )
-                )
+        TimelineView(
+            MetricsTimelineSchedule(
+                from: workoutManager.builder?.startDate ?? Date()
             )
-            Text(
-                153.formatted()
-                + " bpm"
-            )
-            Text(
-                Measurement(
-                    value: 515,
-                    unit: UnitLength.meters
-                ).formatted(
-                    .measurement(
-                        width: .abbreviated,
-                        usage: .road
-                    )
-                )
-            )
+        ) { context in
+            VStack(alignment: .leading) {
+                ElapsedTimeView(elapsedTime: workoutManager.builder?.elapsedTime(at: context.date) ?? 0, showSubseconds: context.cadence == .live)
+                    .foregroundStyle(.yellow)
+                Text(Measurement(value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories)
+                        .formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
+                Text(workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm")
+                Text(Measurement(value: workoutManager.distance, unit: UnitLength.meters).formatted(.measurement(width: .abbreviated, usage: .road)))
+            }
+            .font(.system(.title, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .ignoresSafeArea(edges: .bottom)
+            .scenePadding()
         }
-        .font(.system(.title, design: .rounded)
-            .monospacedDigit()
-            .lowercaseSmallCaps()
-        )
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .ignoresSafeArea(edges: .bottom)
-        .scenePadding()
     }
 }
 
 struct MetricsView_Previews: PreviewProvider {
     static var previews: some View {
         MetricsView()
+    }
+}
+
+private struct MetricsTimelineSchedule: TimelineSchedule {
+    var startDate: Date
+    
+    init(from startDate: Date) {
+        self.startDate = startDate
+    }
+    
+    func entries(from startDate: Date, mode: TimelineScheduleMode) -> PeriodicTimelineSchedule.Entries {
+        PeriodicTimelineSchedule(
+            from: self.startDate,
+            by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0)
+        ).entries(
+            from: startDate,
+            mode: mode
+        )
     }
 }
--- MyWorkouts Watch App/WorkoutManager.swift
+++ MyWorkouts Watch App/WorkoutManager.swift
@@ -48,6 +48,7 @@ class WorkoutManager: NSObject, ObservableObject {
         )
         
         session?.delegate = self
+        builder?.delegate = self
         
 //        Start the workout session and begin data collection.
         let startDate = Date()
@@ -106,6 +107,35 @@ class WorkoutManager: NSObject, ObservableObject {
         showingSummaryView = true
     }
     
+//    MARK: - Workout Metrics
+    @Published var averageHeartRate: Double = 0
+    @Published var heartRate: Double = 0
+    @Published var activeEnergy: Double = 0
+    @Published var distance: Double = 0
+    @Published var workout: HKWorkout?
+    
+    func updateForStatistics(_ statistics: HKStatistics?) {
+        guard let statistics = statistics else { return }
+        
+        DispatchQueue.main.async {
+            switch statistics.quantityType {
+            case HKQuantityType.quantityType(forIdentifier: .heartRate):
+                let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+                self.heartRate = statistics.mostRecentQuantity()?.doubleValue(for: heartRateUnit) ?? 0
+                self.averageHeartRate = statistics.averageQuantity()?.doubleValue(for: heartRateUnit) ?? 0
+            case HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned):
+                let energyUnit = HKUnit.kilocalorie()
+                self.activeEnergy = statistics.sumQuantity()?.doubleValue(for: energyUnit) ?? 0
+            case HKQuantityType.quantityType(forIdentifier: .distanceWalkingRunning),
+                HKQuantityType.quantityType(forIdentifier: .distanceCycling):
+                let meterUnit = HKUnit.meter()
+                self.distance = statistics.sumQuantity()?.doubleValue(for: meterUnit) ?? 0
+            default:
+                return
+            }
+        }
+    }
+    
 }
 
 // MARK: - HKWorkoutSessionDelegate
@@ -123,6 +153,9 @@ extension WorkoutManager: HKWorkoutSessionDelegate {
         if toState == .ended {
             builder?.endCollection(withEnd: date) { (success, error) in
                 self.builder?.finishWorkout { (workout, error) in
+                    DispatchQueue.main.async {
+                        self.workout = workout
+                    }
                 }
             }
         }
@@ -130,5 +163,22 @@ extension WorkoutManager: HKWorkoutSessionDelegate {
     
     func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
         
+    }
+}
+
+// MARK: - HKLiveWorkoutBuilderDelegate
+extension WorkoutManager: HKLiveWorkoutBuilderDelegate {
+    func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) {
+    }
+    
+    func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
+        for type in collectedTypes {
+            guard let quantityType = type as? HKQuantityType else { return }
+            
+            let statistics = workoutBuilder.statistics(for: quantityType)
+            
+//            Update the published values.
+            updateForStatistics(statistics)
+        }
     }
 }
